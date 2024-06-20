977de904d8d604bf2fb89afbdaedfb61bd236c9a
JosimoLopes <joe.lopes@outlook.com>
2024-06-20 12:44:29 -0400

    magnet-effect

--- js/modules/magnet-effect.js
+++ js/modules/magnet-effect.js
@@ -2,9 +2,9 @@ export default function initMagnetEffect() {
   const magnetTargets = document.querySelectorAll('[data-magnet="target"]');
 
   const handleMouseMove = ({ offsetX, offsetY, currentTarget }) => {
-    const [move, x, y] = [30, offsetX, offsetY];
+    const [move, x, y] = [20, offsetX, offsetY];
     const [targetHeight, targetWidth] = [currentTarget.offsetWidth, currentTarget.offsetHeight];
-    const [transX, transY] = [(x / targetWidth) * (move * 2) - move, (y / targetHeight) * (move * 2) - move];
+    const [transX, transY] = [(x / targetWidth) * move, (y / targetHeight) * move];
 
     if (window.innerWidth >= 780) {
       currentTarget.style.transform = `translateX(${transX}px) translateY(${transY}px)`;
