b2b3ef2138b83853a4567e3f4a143b74dfa84ef6
Josimo Lopes <joe.lopes@outlook.com>
2024-08-03 14:29:14 -0400

    transform animaScroll function into a class

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
+import AnimaScroll from "./modules/anima-scroll-controller.js";
 import initCursor from "./modules/cursor-controller.js";
 import initLenisSmoothScroll from "./modules/smooth-scroll-controller.js";
 import initScrollToTarget from "./modules/scroll-target-controller.js";
-import initAnimaScroll from "./modules/anima-scroll-controller.js";
 import initMagnetEffect from "./modules/magnet-effect.js";
 import initLightDarkMode from "./modules/light-dark-mode.js";
 import initObfuscation from "./modules/email-obfuscation.js";
 
+const animaScroll = new AnimaScroll('[data-anima="line"]');
+animaScroll.init();
+
 initCursor();
 initLenisSmoothScroll();
 initScrollToTarget();
-initAnimaScroll();
 initMagnetEffect();
 initLightDarkMode();
 initObfuscation();
